35074b00e873a88faad5da51f2294e7af67c20a2
Dimitriy <d.pikara99@gmail.com>
2022-11-25 17:52:19 +0300

    refactor: added timestamps

--- migrations/003-create-groups.js
+++ migrations/003-create-groups.js
@@ -6,6 +6,14 @@ module.exports = {
       primaryKey: true,
     },
     title: Sequelize.STRING,
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
   }),
   down: (queryInterface) => QueryInterface.dropTable('Groups'),
 };
