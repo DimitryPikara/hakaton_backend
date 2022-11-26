14f771e244dca02fbc476cb0df59b1b930653f78
Dimitriy <d.pikara99@gmail.com>
2022-11-26 12:26:06 +0300

    fix: added settings for cors

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const corsSettings = {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(cors());
+app.use(cors(corsSettings));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
