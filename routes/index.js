d6bd8a96122813aa8a35beb8428592cdc800a3b7
Dimitriy <d.pikara99@gmail.com>
2022-11-26 11:45:44 +0300

    refactor: added auth

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,11 @@ const router = express.Router();
 const usersRouter = require('./Users/users');
 const lectureRouter = require('./Lectures/lectures');
 const groupsRouter = require('./Groups/groups');
+const authRouter = require('./Auth/auth');
 
 router.use('/users', usersRouter);
 router.use('/lecture', lectureRouter);
 router.use('/groups', groupsRouter);
+router.use('/auth', authRouter);
 
 module.exports = router;
