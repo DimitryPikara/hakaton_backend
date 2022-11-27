94687dec4a5ef2ac310e0b0a50b44c652432fa55
dmitri-ponomarev <dmitryp@rollo.com>
2022-11-27 15:06:06 +0300

    fix

--- routes/Lectures/lectures.controller.js
+++ routes/Lectures/lectures.controller.js
@@ -27,31 +27,7 @@ module.exports = {
     try {
       const { groupId } = req.query;
       const { data } = req.body;
-      const lessons = [];
-      data.table.table.slice(2).map((item) => {
-        item.map((lesson, lessonIndex) => {
-          if (lessonIndex !== 0 && lesson) {
-            lessons.push({
-            title: lesson,
-            isOnline: lesson.includes('LMS' || 'Teams'),
-            date: chrono.ru.parseDate(
-            `${item[0].split(',')[1].slice(0, 3)
-            + item[0].split(',')[1].slice(4)
-            },${
-            takeTime(lessonIndex)}`,
-            ),
-            // DONT LOOK, 18+ CONTENT!!!!!!!!!!!!!!!!!!!!!!!
-            teacher: lesson.split(' ').reduce((acc, element, index) => {
-              if (element.includes('.') && element.length === 2 && !acc) {
-                return `${lesson.split(' ')[index - 1]} ${element}${lesson.split(' ')[index + 1]}`;
-              }
-              return acc;
-              }, ''),
-            });
-        }
-      })});
-      console.log(lessons);
-      const lecture = await createLecture(parsedData, groupId);
+      const lecture = await createLecture(data);
       resGenerator(res, 201, lecture);
     } catch (error) {
       resGenerator(res, 400, error);
